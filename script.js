a6a859a528d8b51040ccb6182a96f68118f3ed44
Ana Medeiros <anamedeirosx@gmail.com>
2025-02-18 19:53:17 -0300

    Cronômetro Simples em JS

---
+++ script.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+	var seconds = 0;
+	var tens = 0;
+	var appendTens = document.getElementById("tens");
+	var appendSeconds = document.getElementById("seconds");
+	var buttonStart = document.getElementById("button-start");
+	var buttonStop = document.getElementById("button-stop");
+	var buttonReset = document.getElementById("button-reset");
+	var Interval;
+
+	buttonStart.onclick = function () {
+		clearInterval(Interval);
+		Interval = setInterval(startTimer, 10);
+	};
+
+	buttonStop.onclick = function () {
+		clearInterval(Interval);
+	};
+
+	buttonReset.onclick = function () {
+		clearInterval(Interval);
+		appendTens.innerHTML = "00";
+		appendSeconds.innerHTML = "00";
+	};
+
+	function startTimer() {
+		tens++;
+
+		if (tens <= 9) {
+			appendTens.innerHTML = "0" + tens;
+		} else {
+			appendTens.innerHTML = tens;
+		}
+
+		if (tens > 99) {
+			seconds++;
+			appendSeconds.innerHTML = seconds <= 9 ? "0" + seconds : seconds;
+			tens = 0;
+			appendTens.innerHTML = "00";
+		}
+	}
+};
